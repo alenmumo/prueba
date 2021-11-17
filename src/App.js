b009b01555a63a3cb572ca260817a3c4657456de
alenmumo <alenmumo@gmail.com>
2021-11-17 10:29:49 -0800

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,52 @@
-import logo from './logo.svg';
+
+import React, { useState } from "react";
+//import { makeStyles } from "@material-ui/core/styles";
+//import Paper from "@material-ui/core/Paper";
+//import { PinDropSharp } from "@material-ui/icons";
+
+/*const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1)
+    }
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1)
+  }
+}));*/
+
+var colors = ["green", "red", "blue"];
+
+export default function FloatingActionButtons() {
+
+  const [currentColor, setCurrentColor] = useState(0);
+
+  const changeColor = () => {
+    let nextColor = currentColor + 1;
+    if(nextColor === colors.length) nextColor = 0;
+    console.log("currentColorb: " + currentColor);
+    setCurrentColor(nextColor);
+    console.log("currentColora: " + currentColor);
+  }
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <button onClick={changeColor}>Change color</button>
+      <ChildComponent color={colors[currentColor]}/>
+    </>
+  );
+}
+
+const ChildComponent = (props) => {
+  return (
+    <h1 style={{ background: props.color }}>
+      Child component
+    </h1>
+  );
+};
+
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -22,4 +70,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
